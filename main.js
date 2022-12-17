7d3c61456dcd4d6287ba7d81962ce53c22bb8e93
Neha Dahiya <104566950+nehaadahiya@users.noreply.github.com>
2022-12-17 20:28:25 +0530

    updated mail data credentials

--- main.js
+++ main.js
@@ -96,3 +96,67 @@ function startDate() {
     "/" +
     d.getFullYear();
 }
+
+//for signing up mail entry credentials.
+
+function validation(){
+    var username = document.getElementById('exampleInputName').ariaValueMax;
+    var email = document.getElementById('exampleInputEmail').value;
+    var number = document.getElementById('exampleInputNumber').value;
+    var password = document.getElementById('exampleInputPasssword1').value;
+    var cpassword = document.getElementById('exampleInputPassword2').value;
+    
+    var usercheck = /^[A-Za-z ]{3,50}$/;
+    var emailcheck = /^[A-Za-z0-9._]{3,}@[A-Za-z0-9]{3,}[.]{1}[A-Za-z]{2,6}$/;
+    var numbercheck = /^[6789][0-9]{9}$/;
+    var passwordcheck = /^[A-Za-z0-9_!@#$%^&*]{8,16}$/;
+
+    if (usercheck.test(username)){
+        document.getElementById('errorname').innerHTML=" ";
+
+    }
+    else{
+        document.getElementById('errorname').innerHTML = "Please use only alphabets";
+        return false;
+    }
+
+
+    if (emailcheck.test(email)){
+        document.getElementById('erroremail').innerHTML=" ";
+
+    }
+    else{
+        document.getElementById('erroremail').innerHTML = "Please follow valid email syntax";
+        return false;
+    }
+
+
+    if (numbercheck.test(number)){
+        document.getElementById('errornumber').innerHTML=" ";d
+
+    }
+    else{
+        document.getElementById('errornumber').innerHTML = "Invalid number";
+        return false;
+    }
+
+
+    if (passwordcheck.test(password)){
+        document.getElementById('errorpassword').innerHTML=" ";
+
+    }
+    else{
+        document.getElementById('errorpassword').innerHTML = "Please use a alphabet, numeric, special character";
+        return false;
+    }
+    
+    if (cpassword.match( password)){
+        document.getElementById('errorcpassword').innerHTML=" ";
+
+    }
+    else{
+        document.getElementById('errorcpassword').innerHTML = "Password is not matching";
+        return false;
+    }
+}
+
